aa54384c67505b20ef8c102c56ff1a76721f85f3
Md. Junayed Hasan <101043728+junsan1729@users.noreply.github.com>
2024-03-21 08:05:54 +0530

    Update Contains Duplicate.cpp

--- Contains Duplicate.cpp
+++ Contains Duplicate.cpp
@@ -3,6 +3,7 @@ public:
     bool containsDuplicate(vector<int>& nums) {
       // add all nums element to set 
         unordered_set<int> hash(nums.begin(), nums.end());
+        
       // since set contains only unique elements 
       // size of hashset must be equal or less than size of actual nums 
         return hash.size() != nums.size();
