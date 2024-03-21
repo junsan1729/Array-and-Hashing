13949e77dff63a910d3aa73b492b205b7a117695
Md. Junayed Hasan <101043728+junsan1729@users.noreply.github.com>
2024-03-21 08:02:30 +0530

    Update Two Sum.cpp

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // for element to index mapping 
         unordered_map<int, int> hash;
 
-        // for all numbers, if complememtary exist, return pair of complementary
-        // index and self index else include self value and index pair in hash
+        // for all numbers, if complememtary exist, 
+        // return pair of complementary index and self index 
+        // else include self value and index pair in hash
         for (int i = 0; i < nums.size(); i++) {
             if (hash.count(target - nums[i])) {
                 return {hash[target - nums[i]], i};
@@ -13,6 +15,7 @@ public:
             }
         }
 
+        // return empty indices array if such pair doesn't exist 
         return {};
     }
 };
